c69cdf022bb2129e799e651ce698c30e0bd49722
亞克 <leochiou0927@gmail.com>
2024-06-18 14:51:30 +0800

    6-06-layout-partials

--- 06 Dynamic Content Templating/app.js
+++ 06 Dynamic Content Templating/app.js
@@ -1,40 +1,26 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars').engine;
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.engine(
-    'hbs',
-    expressHbs({
-        layoutsDir: 'views/layouts',
-        defaultLayout: 'main-layout',
-        extname: 'hbs',
-    })
-);
-app.set('view engine', 'hbs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin', adminRoutes.routes);
+app.use("/admin", adminRoutes.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {
-        pageTitle: 'Page Not Found',
-        /**
-         *  BREAKING CHANGE -- will try to render
-         *  views/layouts/main-layout.hbs by
-         *  default
-         */
-        layout: false,
+    res.status(404).render("404", {
+        pageTitle: "Page Not Found",
+        path: "/404",
     });
 });
 
