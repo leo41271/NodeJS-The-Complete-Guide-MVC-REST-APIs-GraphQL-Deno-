d6ab5df32e959ca66f9f8a083af3c42949a005e9
亞克 <leochiou0927@gmail.com>
2024-06-18 10:32:34 +0800

    3-02-parsing-request-bodies

--- 03 Basics/app.js
+++ 03 Basics/app.js
@@ -1,7 +1,9 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     const url = req.url;
+    const method = req.method;
     if (url === '/') {
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
@@ -11,6 +13,22 @@ const server = http.createServer((req, res) => {
         );
         res.write('</html>');
         return res.end();
+    }
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', chunk => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        });
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        console.log('999');
+        return res.end(); // 999 > data > end // 先執行前面三行 再執行return 的end，但 執行end 前會先確保其他先執行，所以data 第二 end 最後
     }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
