38799838923c4a5e8e7c59d42e85b58067ce90c6
亞克 <leochiou0927@gmail.com>
2024-06-18 13:49:56 +0800

    5-03-handling-different-routes

--- 05 Express.js/app.js
+++ 05 Express.js/app.js
@@ -1,13 +1,18 @@
 const express = require('express');
 
 const app = express();
-
-app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next(); // Allow the request to continue to the next middleware in line
+app.get('/favicon.ico', (req, res) => res.status(204));
+app.use('/', (req, res, next) => {
+    console.log('This always runs!');
+    next();
 });
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>The "Add Product" Page</h1>');
+});
+
+app.use('/', (req, res, next) => {
     console.log('In another middleware!');
     res.send('<h1>Hello from Express!</h1>');
 });
