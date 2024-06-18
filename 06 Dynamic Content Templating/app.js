2d4b941f1fd6d92d9801e18404e789639b79e2ae
亞克 <leochiou0927@gmail.com>
2024-06-18 14:50:40 +0800

    6-05-adding-layout-hbs

--- 06 Dynamic Content Templating/app.js
+++ 06 Dynamic Content Templating/app.js
@@ -2,10 +2,19 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars').engine;
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine(
+    'hbs',
+    expressHbs({
+        layoutsDir: 'views/layouts',
+        defaultLayout: 'main-layout',
+        extname: 'hbs',
+    })
+);
+app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
@@ -18,7 +27,15 @@ app.use('/admin', adminRoutes.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', { pageTitle: 'Page Not Found' });
+    res.status(404).render('404', {
+        pageTitle: 'Page Not Found',
+        /**
+         *  BREAKING CHANGE -- will try to render
+         *  views/layouts/main-layout.hbs by
+         *  default
+         */
+        layout: false,
+    });
 });
 
 app.listen(3000);
