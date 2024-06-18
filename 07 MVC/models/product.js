66cbc9340c417cb38779872b6cb90739835d5258
亞克 <leochiou0927@gmail.com>
2024-06-18 14:59:53 +0800

    7-03-fetching-data

--- 07 MVC/models/product.js
+++ 07 MVC/models/product.js
@@ -1,4 +1,8 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
+const rootDir = require("../util/path");
+
+const p = path.join(rootDir, "data", "products.json");
 
 module.exports = class Product {
     constructor(title) {
@@ -6,10 +10,25 @@ module.exports = class Product {
     }
 
     save() {
-        products.push(this);
+        fs.readFile(p, (err, fileContent) => {
+            let products = [];
+            if (!err) {
+                products = JSON.parse(fileContent);
+            }
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        });
     }
 
-    static fetchAll() {
-        return products;
+    static fetchAll(cb) {
+        fs.readFile(p, (err, fileContent) => {
+            if (err) {
+                return cb([]);
+            }
+
+            cb(JSON.parse(fileContent));
+        });
     }
 };
