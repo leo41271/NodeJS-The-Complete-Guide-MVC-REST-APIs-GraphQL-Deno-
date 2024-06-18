8dc0deb15bbc49117097a63608849a4cc08d0d97
亞克 <leochiou0927@gmail.com>
2024-06-18 14:13:49 +0800

    5-11-styling-pages

--- 05 Express.js/routes/shop.js
+++ 05 Express.js/routes/shop.js
@@ -8,7 +8,6 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
     res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-    // res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
 module.exports = router;
