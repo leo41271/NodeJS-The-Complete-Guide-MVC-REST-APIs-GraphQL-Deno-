c09fd24ed756dc369166a142b748b2f538a5361b
亞克 <leochiou0927@gmail.com>
2024-06-20 14:16:06 +0800

    11-13-storing-order-items

--- 11 Sequelize/app.js
+++ 11 Sequelize/app.js
@@ -7,8 +7,10 @@ const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
-const CartItem = require('./models/cart-item');
 const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 const app = express();
 
@@ -41,6 +43,9 @@ User.hasOne(Cart);
 Cart.belongsTo(User);
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
 
 sequelize
     /** -- WORKAROUND FOR FOREIGN KEY ERROR -- */
