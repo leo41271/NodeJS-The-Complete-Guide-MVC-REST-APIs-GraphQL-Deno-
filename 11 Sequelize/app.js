d1463ebb6be4f202423d524af5dbd63564a70fb8
亞克 <leochiou0927@gmail.com>
2024-06-20 14:14:29 +0800

    11-09-many-many

--- 11 Sequelize/app.js
+++ 11 Sequelize/app.js
@@ -7,6 +7,8 @@ const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const CartItem = require('./models/cart-item');
+const Cart = require('./models/cart');
 
 const app = express();
 
@@ -35,9 +37,15 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-    // .sync({ force: true })
+    /** -- WORKAROUND FOR FOREIGN KEY ERROR -- */
+    // .query('SET FOREIGN_KEY_CHECKS = 0', { raw: true })
+    // .then(() => sequelize.sync({ force: true }))
     .sync()
     .then((result) => {
         return User.findByPk(1);
