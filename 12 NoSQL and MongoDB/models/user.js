274c4f4388cb5de4edbcaa33f2e65cb05673e210
亞克 <leochiou0927@gmail.com>
2024-06-22 10:41:15 +0800

    12-09-displaying-cart

--- 12 NoSQL and MongoDB/models/user.js
+++ 12 NoSQL and MongoDB/models/user.js
@@ -18,11 +18,23 @@ class User {
     }
 
     addToCart(product) {
-        // const cartProduct = this.cart.items.findIndex((cp) => {
-        //     return cp._id === product._id;
-        // });
+        const cartProductIndex = this.cart.items.findIndex((cp) => {
+            return cp.productId.toString() === product._id.toString();
+        });
+        let newQuantity = 1;
+        const updatedCartItems = [...this.cart.items];
+
+        if (cartProductIndex >= 0) {
+            newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+            updatedCartItems[cartProductIndex].quantity = newQuantity;
+        } else {
+            updatedCartItems.push({
+                productId: new Object(product._id),
+                quantity: 1,
+            });
+        }
         const updatedCart = {
-            items: [{ productId: product._id, quantity: 1 }],
+            items: updatedCartItems,
         };
         const db = getDb();
         return db
@@ -31,6 +43,27 @@ class User {
                 { _id: new ObjectId(this._id) },
                 { $set: { cart: updatedCart } }
             );
+    }
+
+    getCart() {
+        const db = getDb();
+        const productIds = this.cart.items.map((i) => {
+            return i.productId;
+        });
+        return db
+            .collection('products')
+            .find({ _id: { $in: productIds } })
+            .toArray()
+            .then((products) => {
+                return products.map((p) => {
+                    return {
+                        ...p,
+                        quantity: this.cart.items.find((i) => {
+                            return i.productId.toString() === p._id.toString();
+                        }).quantity,
+                    };
+                });
+            });
     }
 
     static findById(userId) {
