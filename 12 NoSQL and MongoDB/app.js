5d26f04b507170b19fd866fbf47c50608de0dc50
亞克 <leochiou0927@gmail.com>
2024-06-22 10:40:50 +0800

    12-08-add-to-cart

--- 12 NoSQL and MongoDB/app.js
+++ 12 NoSQL and MongoDB/app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
     User.findById('635a9e6a9bc7ce61c0b80a1d')
         .then((user) => {
-            req.user = user;
+            req.user = new User(user.name, user.email, user.cart, user._id);
             next();
         })
         .catch((err) => console.log(err));
