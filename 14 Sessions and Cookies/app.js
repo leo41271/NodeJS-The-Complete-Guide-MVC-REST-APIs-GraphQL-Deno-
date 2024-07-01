1f06a402fabb2114303aafce165bbae16c6ccc32
亞克 <leochiou0927@gmail.com>
2024-07-01 13:27:43 +0800

    14-04-using-session-middleware

--- 14 Sessions and Cookies/app.js
+++ 14 Sessions and Cookies/app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -18,6 +19,13 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+    session({
+        secret: 'my secret',
+        resave: false,
+        saveUninitialized: false,
+    })
+);
 
 app.use((req, res, next) => {
     User.findById('6606e689fb4c4cbf7dd95380')
