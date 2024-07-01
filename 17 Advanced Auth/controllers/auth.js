660fa2ac02b58c34101260a3c9354b1406ab74c5
亞克 <leochiou0927@gmail.com>
2024-07-01 13:42:40 +0800

    17-03-password-reset-form

--- 17 Advanced Auth/controllers/auth.js
+++ 17 Advanced Auth/controllers/auth.js
@@ -179,3 +179,31 @@ exports.postReset = (req, res, next) => {
             });
     });
 };
+
+exports.getNewPassword = (req, res, next) => {
+    const token = req.params.token;
+    User.findOne({
+        resetToken: token,
+        resetTokenExpiration: { $gt: Date.now() },
+    })
+        .then((user) => {
+            if (!user) {
+                return res.redirect('/');
+            }
+            let message = req.flash('error');
+            if (message.length > 0) {
+                message = message[0];
+            } else {
+                message = null;
+            }
+            res.render('auth/new-password', {
+                path: '/new-password',
+                pageTitle: 'New Password',
+                errorMessage: message,
+                userId: user._id.toString(),
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+};
