02148eeeedf8f69aaa0a729b5e8c4fe27fe600b1
亞克 <leochiou0927@gmail.com>
2024-07-01 13:41:37 +0800

    17-01-resetting-password

--- 17 Advanced Auth/controllers/auth.js
+++ 17 Advanced Auth/controllers/auth.js
@@ -1,3 +1,5 @@
+const crypto = require('crypto');
+
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
@@ -121,5 +123,19 @@ exports.postLogout = (req, res, next) => {
     req.session.destroy((err) => {
         console.log(err);
         res.redirect('/');
+    });
+};
+
+exports.getReset = (req, res, next) => {
+    let message = req.flash('error');
+    if (message.length > 0) {
+        message = message[0];
+    } else {
+        message = null;
+    }
+    res.render('auth/reset', {
+        path: '/reset',
+        pageTitle: 'Reset Password',
+        errorMessage: message,
     });
 };
