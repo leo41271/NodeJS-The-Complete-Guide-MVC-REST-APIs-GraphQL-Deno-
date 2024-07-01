867fce56cffa3be7b857096488363cf86d3286dd
亞克 <leochiou0927@gmail.com>
2024-07-01 13:28:10 +0800

    14-05-mongodb-sessions

--- 14 Sessions and Cookies/app.js
+++ 14 Sessions and Cookies/app.js
@@ -4,11 +4,22 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+/** REPLACE CONNECTION STRING IF USING ATLAS
+ *  "mongodb+srv://<username>:<password>@<cluster-id>.mongodb.net/<dbName>?retryWrites=true&authSource=admin"
+ */
+const MONGODB_URI =
+    'mongodb://127.0.0.1:27017/shop?retryWrites=true&authSource=admin';
+
 const app = express();
+const store = new MongoDBStore({
+    uri: MONGODB_URI,
+    collection: 'sessions',
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -24,6 +35,7 @@ app.use(
         secret: 'my secret',
         resave: false,
         saveUninitialized: false,
+        store: store,
     })
 );
 
@@ -42,11 +54,8 @@ app.use(authRoutes);
 
 app.use(errorController.get404);
 
-/** REPLACE CONNECTION STRING IF USING ATLAS
- *  "mongodb+srv://<username>:<password>@<cluster-id>.mongodb.net/<dbName>?retryWrites=true&authSource=admin"
- */
 mongoose
-    .connect('mongodb://127.0.0.1:27017/shop?retryWrites=true&authSource=admin')
+    .connect(MONGODB_URI)
     .then((result) => {
         User.findOne().then((user) => {
             if (!user) {
