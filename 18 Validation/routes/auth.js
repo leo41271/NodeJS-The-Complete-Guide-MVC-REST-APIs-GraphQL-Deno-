b8110da6f6393504642ebc4ba9fa67d434f9c4fa
亞克 <leochiou0927@gmail.com>
2024-07-01 13:46:56 +0800

    18-04-checking-field-equality

--- 18 Validation/routes/auth.js
+++ 18 Validation/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 /** note we are importing from the base package */
-const { check } = require('express-validator');
+const { check, body } = require('express-validator');
 
 const authController = require('../controllers/auth');
 
@@ -14,15 +14,29 @@ router.post('/login', authController.postLogin);
 
 router.post(
     '/signup',
-    check('email')
-        .isEmail()
-        .withMessage('Please enter a valid email.')
-        .custom((value, { req }) => {
-            if (value === 'test@test.com') {
-                throw new Error('This email address is forbidden.');
+    [
+        check('email')
+            .isEmail()
+            .withMessage('Please enter a valid email.')
+            .custom((value, { req }) => {
+                if (value === 'test@test.com') {
+                    throw new Error('This email address is forbidden.');
+                }
+                return true;
+            }),
+        body(
+            'password',
+            'Please enter a password with only letters and numbers and at least 5 characters.'
+        )
+            .isLength({ min: 5 })
+            .isAlphanumeric(),
+        body('confirmPassword').custom((value, { req }) => {
+            if (value !== req.body.password) {
+                throw new Error('Passwords have to match!');
             }
             return true;
         }),
+    ],
     authController.postSignup
 );
 
