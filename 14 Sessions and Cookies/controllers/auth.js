5c14349466d8de64d018dd36cf4aef1bca0dbf2f
亞克 <leochiou0927@gmail.com>
2024-07-01 13:27:18 +0800

    14-03-configuring-cookies

--- 14 Sessions and Cookies/controllers/auth.js
+++ 14 Sessions and Cookies/controllers/auth.js
@@ -1,12 +1,17 @@
 exports.getLogin = (req, res, next) => {
+    // const isLoggedIn = req
+    //     .get('Cookie')
+    //     .split(';')[1]
+    //     .trim()
+    //     .split('=')[1] === 'true';
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
-        isAuthenticated: req.isLoggedIn,
+        isAuthenticated: false,
     });
 };
 
 exports.postLogin = (req, res, next) => {
-    req.isLoggedIn = true;
+    res.setHeader('Set-Cookie', 'loggedIn=true; HttpOnly');
     res.redirect('/');
 };
