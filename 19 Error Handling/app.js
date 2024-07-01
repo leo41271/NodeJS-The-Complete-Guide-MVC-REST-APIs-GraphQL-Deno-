6b7b948516f9d16a684ba042310ec38449d09842
亞克 <leochiou0927@gmail.com>
2024-07-01 13:57:54 +0800

    19-04-status-codes

--- 19 Error Handling/app.js
+++ 19 Error Handling/app.js
@@ -54,6 +54,13 @@ app.use(csrfProtection.doubleCsrfProtection);
 app.use(flash());
 
 app.use((req, res, next) => {
+    res.locals.isAuthenticated = req.session.isLoggedIn;
+    res.locals.csrfToken = req.csrfToken();
+    next();
+});
+
+app.use((req, res, next) => {
+    // throw new Error('Sync Dummy');
     if (!req.session.user) {
         return next();
     }
@@ -66,14 +73,8 @@ app.use((req, res, next) => {
             next();
         })
         .catch((err) => {
-            throw new Error(err);
+            next(new Error(err));
         });
-});
-
-app.use((req, res, next) => {
-    res.locals.isAuthenticated = req.session.isLoggedIn;
-    res.locals.csrfToken = req.csrfToken();
-    next();
 });
 
 app.use('/admin', adminRoutes);
@@ -86,7 +87,12 @@ app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
     // res.status(error.httpStatusCode).render(....)
-    res.redirect('/500');
+    // res.redirect('/500');
+    res.status(500).render('500', {
+        pageTitle: 'Error!',
+        path: '/500',
+        isAuthenticated: req.session.isLoggedIn,
+    });
 });
 
 mongoose
--- 19 Error Handling/controllers/auth.js
+++ 19 Error Handling/controllers/auth.js
@@ -114,7 +114,11 @@ exports.postLogin = (req, res, next) => {
                     res.redirect('/login');
                 });
         })
-        .catch((err) => console.log(err));
+        .catch((err) => {
+            const error = new Error(err);
+            error.httpStatusCode = 500;
+            return next(error);
+        });
 };
 
 exports.postSignup = (req, res, next) => {
@@ -162,7 +166,9 @@ exports.postSignup = (req, res, next) => {
             });
         })
         .catch((err) => {
-            console.log(err);
+            const error = new Error(err);
+            error.httpStatusCode = 500;
+            return next(error);
         });
 };
 
@@ -222,7 +228,9 @@ exports.postReset = (req, res, next) => {
                 });
             })
             .catch((err) => {
-                console.log(err);
+                const error = new Error(err);
+                error.httpStatusCode = 500;
+                return next(error);
             });
     });
 };
@@ -252,7 +260,9 @@ exports.getNewPassword = (req, res, next) => {
             });
         })
         .catch((err) => {
-            console.log(err);
+            const error = new Error(err);
+            error.httpStatusCode = 500;
+            return next(error);
         });
 };
 
@@ -281,6 +291,8 @@ exports.postNewPassword = (req, res, next) => {
             res.redirect('/login');
         })
         .catch((err) => {
-            console.log(err);
+            const error = new Error(err);
+            error.httpStatusCode = 500;
+            return next(error);
         });
 };
