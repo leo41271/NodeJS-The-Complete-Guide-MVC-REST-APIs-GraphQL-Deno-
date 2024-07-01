c26d5ebd0761e496675564afefa12c66c5efff8c
亞克 <leochiou0927@gmail.com>
2024-07-01 13:57:09 +0800

    19-02-returning-error-page。 (error-playground.js is not use anymore. can delete or in other modules that i commit will not exist )

--- 19 Error Handling/controllers/admin.js
+++ 19 Error Handling/controllers/admin.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+
 const { validationResult } = require('express-validator');
 
 const Product = require('../models/product');
@@ -23,7 +25,7 @@ exports.postAddProduct = (req, res, next) => {
     if (!errors.isEmpty()) {
         return res.status(422).render('admin/edit-product', {
             pageTitle: 'Add Product',
-            path: '/admin/edit-product',
+            path: '/admin/add-product',
             editing: false,
             hasError: true,
             product: {
@@ -38,6 +40,7 @@ exports.postAddProduct = (req, res, next) => {
     }
 
     const product = new Product({
+        _id: new mongoose.Types.ObjectId('6606e7bd39bee6b6fd582558'),
         title: title,
         price: price,
         description: description,
@@ -52,7 +55,21 @@ exports.postAddProduct = (req, res, next) => {
             res.redirect('/admin/products');
         })
         .catch((err) => {
-            console.log(err);
+            // return res.status(500).render('admin/edit-product', {
+            //     pageTitle: 'Add Product',
+            //     path: '/admin/add-product',
+            //     editing: false,
+            //     hasError: true,
+            //     product: {
+            //         title: title,
+            //         imageUrl: imageUrl,
+            //         price: price,
+            //         description: description,
+            //     },
+            //     errorMessage: 'Database operation failed, please try again.',
+            //     validationErrors: [],
+            // });
+            res.redirect('/500');
         });
 };
 
