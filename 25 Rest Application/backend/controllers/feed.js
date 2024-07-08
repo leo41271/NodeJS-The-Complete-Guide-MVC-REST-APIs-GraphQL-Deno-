02f76a12595fccc18bcf835706e2b794117f7349
亞克 <leochiou0927@gmail.com>
2024-07-08 10:50:56 +0800

    25-05-fetching-single-post

--- 25 Rest Application/backend/controllers/feed.js
+++ 25 Rest Application/backend/controllers/feed.js
@@ -3,20 +3,19 @@ const { validationResult } = require('express-validator');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
-    res.status(200).json({
-        posts: [
-            {
-                _id: '1',
-                title: 'First Post',
-                content: 'This is the first post!',
-                imageUrl: 'images/duck.png',
-                creator: {
-                    name: 'Maximilian',
-                },
-                createdAt: new Date(),
-            },
-        ],
-    });
+    Post.find()
+        .then((posts) => {
+            res.status(200).json({
+                message: 'Fetched posts successfully.',
+                posts: posts,
+            });
+        })
+        .catch((err) => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
 };
 
 exports.createPost = (req, res, next) => {
@@ -43,6 +42,28 @@ exports.createPost = (req, res, next) => {
             console.log(result);
             res.status(201).json({
                 message: 'Post created successfully!',
+                post: post,
+            });
+        })
+        .catch((err) => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
+};
+
+exports.getPost = (req, res, next) => {
+    const postId = req.params.postId;
+    Post.findById(postId)
+        .then((post) => {
+            if (!post) {
+                const error = new Error('Could not find post.');
+                error.statusCode = 404;
+                throw error;
+            }
+            res.status(200).json({
+                message: 'Post fetched.',
                 post: post,
             });
         })
