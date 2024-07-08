1545e61b9b9697686a85bac1be6e88e6f717f85d
亞克 <leochiou0927@gmail.com>
2024-07-08 10:50:40 +0800

    25-04-static-images

--- 25 Rest Application/backend/app.js
+++ 25 Rest Application/backend/app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -8,6 +10,7 @@ const app = express();
 
 // app.use(bodyParser.urlEncoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -23,6 +26,13 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode;
+    const message = error.message;
+    res.status(status).json({ message: message });
+});
 
 /** REPLACE CONNECTION STRING IF USING ATLAS
  *  "mongodb+srv://<username>:<password>@<cluster-id>.mongodb.net/<dbName>?retryWrites=true&authSource=admin"
