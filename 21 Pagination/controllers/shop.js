eb17a24fb4c2e0dc3a99a9a1f41214f7189d3a52
亞克 <leochiou0927@gmail.com>
2024-07-08 09:48:23 +0800

    21-03-dynamic-page-btns

--- 21 Pagination/controllers/shop.js
+++ 21 Pagination/controllers/shop.js
@@ -43,7 +43,7 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-    const page = req.query.page;
+    const page = +req.query.page || 1;
     let totalItems;
 
     Product.find()
@@ -59,11 +59,11 @@ exports.getIndex = (req, res, next) => {
                 prods: products,
                 pageTitle: 'Shop',
                 path: '/',
-                totalProducts: totalItems,
+                currentPage: page,
                 hasNextPage: ITEMS_PER_PAGE * page < totalItems,
                 hasPreviousPage: page > 1,
                 nextPage: page + 1,
-                prevPage: page - 1,
+                previousPage: page - 1,
                 lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
             });
         })
