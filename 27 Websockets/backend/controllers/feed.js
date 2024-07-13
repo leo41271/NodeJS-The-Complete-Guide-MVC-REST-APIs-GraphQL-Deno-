67a9eb607a9bd28b53b293a59b3251087aaab871
亞克 <leochiou0927@gmail.com>
2024-07-13 12:17:57 +0800

    27-03-updating-posts-all

--- 27 Websockets/backend/controllers/feed.js
+++ 27 Websockets/backend/controllers/feed.js
@@ -60,7 +60,10 @@ exports.createPost = async (req, res, next) => {
         await user.save();
         io.getIO().emit('posts', {
             action: 'create',
-            post: post,
+            post: {
+                ...post._doc,
+                creator: { _id: req.userId, name: user.name },
+            },
         });
         res.status(201).json({
             message: 'Post created successfully!',
@@ -122,13 +125,13 @@ exports.updatePost = async (req, res, next) => {
         throw error;
     }
     try {
-        const post = await Post.findById(postId);
+        const post = await Post.findById(postId).populate('creator');
         if (!post) {
             const error = new Error('Could not find post.');
             error.statusCode = 404;
             throw error;
         }
-        if (post.creator.toString() !== req.userId) {
+        if (post.creator._id.toString() !== req.userId) {
             const error = new Error('Not authorized.');
             error.statusCode = 403;
             throw error;
@@ -140,6 +143,7 @@ exports.updatePost = async (req, res, next) => {
         post.imageUrl = imageUrl;
         post.content = content;
         const result = await post.save();
+        io.getIO().emit('posts', { action: 'update', post: result });
         res.status(200).json({ message: 'Post updated!', post: result });
     } catch (err) {
         if (!err.statusCode) {
