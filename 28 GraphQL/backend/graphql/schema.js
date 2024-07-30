b797dbde261b36cbcbfb69d3ead180a35cf118b6
亞克 <leochiou0927@gmail.com>
2024-07-30 14:38:31 +0800

    28-03-mutation-resolver

--- 28 GraphQL/backend/graphql/schema.js
+++ 28 GraphQL/backend/graphql/schema.js
@@ -26,11 +26,16 @@ module.exports = buildSchema(`
         password: String!
     }
 
+    type RootQuery {
+        hello: String
+    }
+
     type RootMutation {
         createUser(userInput: UserData): User
     }
 
     schema {
+        query: RootQuery
         mutation: RootMutation
     }
 `);
