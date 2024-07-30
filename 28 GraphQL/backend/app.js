ed36292e1e000bee70057daa7320e4998fce1408
亞克 <leochiou0927@gmail.com>
2024-07-30 14:42:09 +0800

    28-11-uploading-images

--- 28 GraphQL/backend/app.js
+++ 28 GraphQL/backend/app.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const path = require('path');
 
 const express = require('express');
@@ -64,6 +65,23 @@ app.use((req, res, next) => {
 
 app.use(auth);
 
+app.put('/post-image', (req, res, next) => {
+    if (!req.isAuth) {
+        throw new Error('Not authenticated!');
+    }
+    if (!req.file) {
+        return res.status(200).json({ message: 'No file provided!' });
+    }
+    if (req.body.oldPath) {
+        clearImage(req.body.oldPath);
+    }
+    return res.status(201).json({
+        message: 'File stored.',
+        /** REPLACE ALL '\' with '/' */
+        filePath: req.file.path.replace(/\\/g, '/'),
+    });
+});
+
 /** EXPRESS-GRAPHIQL-EXPLORER PACKAGE */
 /** note: /graphiql endpoint */
 app.use(
@@ -117,3 +135,8 @@ mongoose
         app.listen(8080);
     })
     .catch((err) => console.log(err));
+
+function clearImage(filePath) {
+    filePath = path.join(__dirname, '..', filePath);
+    fs.unlink(filePath, (err) => console.log(err));
+}
