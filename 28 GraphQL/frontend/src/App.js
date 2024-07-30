0774ef1d0d65e08892eeeff12b2507a176e6348b
亞克 <leochiou0927@gmail.com>
2024-07-30 14:39:25 +0800

    28-05-connecting-frontend

--- 28 GraphQL/frontend/src/App.js
+++ 28 GraphQL/frontend/src/App.js
@@ -112,29 +112,37 @@ class App extends Component {
     signupHandler = (event, authData) => {
         event.preventDefault();
         this.setState({ authLoading: true });
-        console.log(authData);
-        fetch('http://localhost:8080/auth/signup', {
-            method: 'PUT',
+        const graphqlQuery = {
+            query: `
+                mutation {
+                    createUser(userInput: {
+                        name:"${authData.signupForm.name.value}",
+                        email: "${authData.signupForm.email.value}",
+                        password: "${authData.signupForm.password.value}"}
+                    ){
+                        name
+                        email
+                    }
+                }
+            `,
+        };
+        fetch('http://localhost:8080/graphql', {
+            method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({
-                email: authData.signupForm.email.value,
-                password: authData.signupForm.password.value,
-                name: authData.signupForm.name.value,
-            }),
+            body: JSON.stringify(graphqlQuery),
         })
             .then((res) => {
-                if (res.status === 422) {
-                    throw new Error("Validation failed. Make sure the email address isn't used yet!");
-                }
-                if (res.status !== 200 && res.status !== 201) {
-                    console.log('Error!');
-                    throw new Error('Creating a user failed!');
-                }
                 return res.json();
             })
             .then((resData) => {
+                if (resData.errors && resData.errors.status === 422) {
+                    throw new Error("Validation failed. Make sure the email address isn't used yet!");
+                }
+                if (resData.errors) {
+                    throw new Error('User creation failed.');
+                }
                 console.log(resData);
                 this.setState({ isAuth: false, authLoading: false });
                 /** CUSTOM HOC -- withRouter */
