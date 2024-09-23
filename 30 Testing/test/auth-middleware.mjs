40e425f192e0b5e47ca10561efe5b3ab25c3bf0b
leo41271 <leochiou0927@gmail.com>
2024-09-23 15:43:01 +0800

    30-03-multiple-tests

--- 30 Testing/test/auth-middleware.mjs
+++ 30 Testing/test/auth-middleware.mjs
@@ -3,13 +3,24 @@ import { expect } from 'chai';
 /** MUST ADD FILE EXTENSIONS WHEN USING MJS */
 import authMiddleware from '../middleware/is-auth.js';
 
-it('should throw an error if no authorization header is present', function () {
-    const req = {
-        get: function () {
-            return null;
-        },
-    };
-    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
-        'Not authenticated.'
-    );
+describe('Auth middleware', function () {
+    it('should throw an error if no authorization header is present', function () {
+        const req = {
+            get: function () {
+                return null;
+            },
+        };
+        expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
+            'Not authenticated.'
+        );
+    });
+
+    it('should throw an error if the authorization header is only one string', function () {
+        const req = {
+            get: function () {
+                return 'xyz';
+            },
+        };
+        expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+    });
 });
